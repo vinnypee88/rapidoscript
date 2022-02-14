997f333d29240d4f3953cba86ce56e57d9c4e7b7
Vinod Patel <v_p88@hotmail.com>
2022-02-14 11:26:52 +0000

    Set up a basic Stopclock with buttons for testing

---
+++ components/Words.js
@@ -0,0 +1,16 @@
+const Words = () => {
+  return (
+    <div className="container">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quis,
+        expedita nulla aliquid, dolor quos inventore earum velit, veritatis
+        adipisci delectus voluptate ad aut necessitatibus? Perferendis ipsam
+        quae, nulla sed nostrum reprehenderit inventore maiores est eos! Alias
+        impedit optio nisi perferendis eveniet pariatur sapiente fugiat at
+        laudantium, laborum animi? Officiis.
+      </p>
+    </div>
+  );
+};
+
+export default Words;
