666f48bdaa2ba4d43ced53328de9b735566fb343
Vinod Patel <v_p88@hotmail.com>
2022-02-16 07:40:21 +0000

    Added basic navbar with title and no links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,34 +1,10 @@
 const Navbar = () => {
   return (
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">
-              Home
-            </a>
-            <a class="nav-link" href="#">
-              Features
-            </a>
-            <a class="nav-link" href="#">
-              Pricing
-            </a>
-          </div>
-        </div>
+    <nav className="navbar">
+      <div className="container-fluid d-flex justify-content-center">
+        <h1 className="text-primary bg-warning p-3" href="#">
+          Rapidoscript
+        </h1>
       </div>
     </nav>
   );
