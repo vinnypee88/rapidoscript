97bdbafe3e0e221f60784e20e033f95a66414771
Vinod Patel <v_p88@hotmail.com>
2022-02-16 07:38:32 +0000

    Added banner with instructions and disclaimer

---
+++ components/Banner.js
@@ -0,0 +1,17 @@
+const Banner = () => {
+  return (
+    <div className="container text-center">
+      <h2>Instructions</h2>
+      <p>
+        Type the javascript related words as fast as you can. Slowest person
+        buys everyone icecream.
+      </p>
+      <p className="fst-italic text-danger">
+        Disclaimer - This is only for fun. This will not make you a better
+        programmer
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
